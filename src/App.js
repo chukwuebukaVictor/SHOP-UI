a3a405ce3b76c4cb9792c5640e7bf7bae8d70238
Chukwuebuka <Chukwuebuka.victor.ozoede@gmail.com>
2023-04-29 17:55:28 +0100

    Display list of products

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 // import logo from './logo.svg';
 // import './App.css';
 
+import data from './data';
+
 function App() {
   return (
     <div>
       <header>
         <a href="/">Ikoyi-Mall</a>
       </header>
-      <main>List Products</main>
+      <main>
+        <h1>Featured Product</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div key={product.slug} className="product">
+              <img src={product.image} alt={product.name} />
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>
+                  <strong>{product.price}</strong>
+                </p>
+                <button>Add to cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
